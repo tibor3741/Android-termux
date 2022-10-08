81638ff87f1b28a9a1b20704e4c66ff1f34d85a8
Tibor <36704519+tibor3741@users.noreply.github.com>
2022-10-08 16:15:12 +0200

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-pkg install bash-completion nano w3m python php neofetch termux-api openssh -y
-pkg update -y
-pkg upgrade -y
-ln -s  ../usr/etc/bash.bashrc .bashrc
-#bashrc beírás
-echo '#índításkor lefut'>>.bashrc
-echo 'clear'>>.bashrc
-echo 'neofetch'>>.bashrc 
-echo "date">>.bashrc
-echo "#Saját aliasok">>.bashrc
-echo "alias rm='rm -rf'">>.bashrc
-echo "alias ls='ls -a'">>.bashrc
-echo "alias c='clear'">>.bashrc
-clear
-exit
+pkg update
+pkg install neofetch python perl tmux vim vifm  zsh curl wget  openssh -y 
+pkg autoclean
+termux-setup-storage
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+#zsh
+git clone https://github.com/dovahkiin0424/magyarch-zsh
+#vifm
+git clone https://gitlab.com/dwt1/dotfiles/-/tree/master/.config/vifm
+#nvim
+git clone https://gitlab.com/dwt1/dotfiles/-/tree/master/.config/nvim
+#aliases
+wget https://github.com/magyarch/magyarch-ratpoison/blob/main/.config/aliasrc
+#install zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
